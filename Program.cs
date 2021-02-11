337a71824c8fca1c65296fb6311496da1a2258e0
Matas Strimaitis <matasmatasp@gmail.com>
2021-02-11 16:24:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
